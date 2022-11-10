3ccdff29022911641028d5685f068875dbb65555
gemdoq <gemdoq@gmail.com>
2022-11-10 12:56:19 +0900

    imple list()

--- src/main/java/com/example/bbsbuild/dockertest/controller/ArticleController.java
+++ src/main/java/com/example/bbsbuild/dockertest/controller/ArticleController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -48,5 +49,17 @@ public class ArticleController {
         } else {
             return "articles/error";
         }
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "articles/list";
+    }
+
+    @GetMapping("")
+    public String index() {
+        return "redirect:/articles/list";
     }
 }
