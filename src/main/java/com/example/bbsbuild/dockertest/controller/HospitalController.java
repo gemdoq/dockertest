4fb152e878da8c0683e37bbb7be61202d1f9f601
gemdoq <gemdoq@gmail.com>
2022-11-14 09:26:39 +0900

    feature: create index, seperate header into head, nav, script, footer

--- src/main/java/com/example/bbsbuild/dockertest/controller/HospitalController.java
+++ src/main/java/com/example/bbsbuild/dockertest/controller/HospitalController.java
@@ -9,7 +9,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/hospitals")
@@ -19,7 +22,18 @@ public class HospitalController {
 
     private final HospitalRepository hospitalRepository;
 
-    @GetMapping("")
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Integer id, Model model) {
+        Optional<Hospital> optionalHospital = hospitalRepository.findById(id);
+        if (optionalHospital.isPresent()) {
+            model.addAttribute("hospital", optionalHospital.get());
+            return "hospitals/show";
+        } else {
+            return "hospitals/error";
+        }
+    }
+
+    @GetMapping("/list")
     public String list(Model model, Pageable pageable) {
         Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
         log.info("size:{}", hospitals.getSize());
@@ -27,5 +41,10 @@ public class HospitalController {
         model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
         model.addAttribute("next", pageable.next().getPageNumber());
         return "hospitals/list";
+    }
+
+    @GetMapping("")
+    public String list(Model model) {
+        return "redirect:/hospitals/list";
     }
 }
